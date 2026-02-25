1a52f95805288c32ca733eda54f01d2862e79d55
btnav <174347826+bt-nav@users.noreply.github.com>
2026-02-25 14:17:40 +0000

    Ensure gazebo is at z=0 matching rviz

--- src/mycobot_280pi_gazebo/launch/mycobot_280pi.gazebo.launch.py
+++ src/mycobot_280pi_gazebo/launch/mycobot_280pi.gazebo.launch.py
@@ -139,7 +139,7 @@ def generate_launch_description():
 
     declare_z_cmd = DeclareLaunchArgument(
         name='z',
-        default_value='0.05',
+        default_value='0.0',
         description='z component of initial position, meters')
 
     declare_roll_cmd = DeclareLaunchArgument(
